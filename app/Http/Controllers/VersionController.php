88807337745ceabe90d63f50f70eac8704669063
Wkan <wangkan1119@gmail.com>
2020-02-23 16:58:14 +0800

    添加版本接口用于测试

---
+++ app/Http/Controllers/VersionController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class VersionController extends Controller
+{
+    public function index()
+    {
+        return response()->json(['version' => 'this is debug version']);
+    }
+}
